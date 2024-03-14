958061243dd7021f0f85fe5c3dbbfe1e8597b715
Florian Remberger <flo.remberger@gmail.com>
2024-03-14 18:31:06 +0100

    Upload changes which worked on server

--- backend/db/database.py
+++ backend/db/database.py
@@ -8,10 +8,10 @@ load_dotenv()
 POSTGRES_DB = os.getenv('POSTGRES_DB')
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST') 
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+#POSTGRES_HOST = os.getenv('POSTGRES_HOST') 
+#POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
